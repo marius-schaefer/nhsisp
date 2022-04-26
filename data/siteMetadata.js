caf6617c6b3f5991102a64d620c8be6bb4f28776
marius-schaefer <marius.schaefer2005@gmail.com>
2022-04-26 21:47:02 +0300

    Initial Commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Student Mirror',
+  author: 'Student Mirror',
+  headerTitle: 'Student Mirror',
+  description: 'The Student Mirror Website',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/marius-schaefer/studentmirrorv2',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
